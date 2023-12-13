7a0520006a91f8564b88e742074697e86bed7aa8
Mustafa Kilic <mustafa.kilic@tirport.com>
2023-12-14 02:36:45 +0300

    The filing structure for user services was organized and the necessary services were created.

--- src/logic/users/users.module.ts
+++ src/logic/users/users.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
 
 @Module({
   providers: [UsersService],
-  controllers: [UsersController]
+  exports: [UsersService],
 })
 export class UsersModule {}
