c2080be06fc94266caeae18bcc3b432d444d6f3f
Mustafa Kilic <mustafa.kilic@tirport.com>
2023-12-15 03:36:22 +0300

    new apiError enum added.

--- src/enum/apiError.enum.ts
+++ src/enum/apiError.enum.ts
@@ -3,4 +3,6 @@ export enum ApiErrorEnum {
   api_error_credential_invalid = 1,
   api_error_user_already_exist = 2,
   api_error_invalid_input_data = 4,
+  api_error_category_already_exists = 5,
+  api_error_unauthorized = 6,
 }
